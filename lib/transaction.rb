06440b084e71b6562620b310838d458ddebbe053
Alastair Gilles <agilles91@gmail.com>
2020-01-27 16:31:26 +0100

    refactor: combine deposit and withdrawal as a transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,8 +1,23 @@
 class Transaction
-  attr_reader :amount, :date
+  attr_reader :type, :amount, :date
 
-  def initialize(amount, date)
+  def initialize(type, amount, date)
+    @type = type
     @amount = amount
     @date = date
+  end
+
+  def balance_after balance_before
+    case type
+    when :deposit then balance_before + amount
+    when :withdrawal then balance_before - amount
+    end
+  end
+
+  def balance_before balance_after
+    case type
+    when :deposit then balance_after - amount
+    when :withdrawal then balance_after + amount
+    end
   end
 end
--- spec/units/transaction_spec.rb
+++ spec/units/transaction_spec.rb
@@ -1,6 +1,66 @@
 require 'transaction'
-require_relative 'transaction_examples'
+require 'time'
+
+shared_examples 'a transaction' do
+  describe '#amount' do
+    it 'should return the amount involved in the transaction' do
+      expect(subject.amount).to eq 100
+    end
+  end
+
+  describe '#date' do
+    it 'should return the date the transaction occured' do
+      expect(subject.date).to eq Time.parse('2019-05-13')
+    end
+  end
+end
 
 RSpec.describe Transaction do
-  it_behaves_like "a transaction"
+  describe ':deposit' do
+    subject { Transaction.new(:deposit, 100, Time.parse('2019-05-13')) }
+
+    it_behaves_like 'a transaction'
+
+    describe "#type" do
+      it "should return :deposit" do
+        expect(subject.type).to eq :deposit
+      end
+    end
+
+    describe "#balance_after" do
+      it "should return the balance after the transaction" do
+        expect(subject.balance_after(400)).to eq 500
+      end
+    end
+
+    describe "#balance_before" do
+      it "should return the balance before the transaction" do
+        expect(subject.balance_before(500)).to eq 400
+      end
+    end
+  end
+
+  describe ':withdrawal' do
+    subject { Transaction.new(:withdrawal, 100, Time.parse('2019-05-13')) }
+
+    it_behaves_like 'a transaction'
+
+    describe "#type" do
+      it "should return withdrawal" do
+        expect(subject.type).to eq :withdrawal
+      end
+    end
+
+    describe "#balance_after" do
+      it "should return the balance after the transaction" do
+        expect(subject.balance_after(400)).to eq 300
+      end
+    end
+
+    describe "#balance_before" do
+      it "should return the balance before the transaction" do
+        expect(subject.balance_before(300)).to eq 400
+      end
+    end
+  end
 end
